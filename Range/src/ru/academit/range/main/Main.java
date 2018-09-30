a886029300a7a5f3b22421dd65fa9b025284c0e5
konstantinbiriukov <kostya-leon@mail.ru>
2018-09-30 21:53:07 +0700

    getUnionRange

--- Range/src/ru/academit/range/main/Main.java
+++ Range/src/ru/academit/range/main/Main.java
@@ -7,9 +7,8 @@ public class Main {
         Range range = new Range(2, 9);
         Range rangeNew = new Range(5, 12);
 
-
         range.getIntersectionRange(rangeNew);
-        range.getUnionRange(rangeNew);
+     //   range.getUnionRange(rangeNew);
 
        // System.out.println("Difference = " + Arrays.toString(range.getDifferenceRange(range)));
     }
