0d37a07a75ca1295b6dc01f60f68995e5a01460f
konstantinbiriukov <kostya-leon@mail.ru>
2018-11-14 05:33:22 +0700

    arraylist

---
+++ ArrayList/src/ru/academit/arrayList/ArrayList.java
@@ -0,0 +1,369 @@
+// Сделать свою реализацию списка ArrayList<T> (в C# - List<T>), сделать его generic’ом.
+// Необходимо реализовать интерфейс List<T> (в C# - IList<T>).
+// Нужно реализовать специфичный конструктор, принимающий вместимость,
+// а также методы ensureCapacity и trimToSize.
+
+// 1 size
+// 2 isEmpty
+// 3 contains
+// 4 iterator
+// 5 toArray
+// 6 toArray
+// 7 add
+// 8 remove
+// 9 containsAll
+// 10 addAll
+// 11 addAll
+// 12 removeAll
+// 13 retainAll
+// 14 clear
+// 15 get
+// 16 set
+// 17 add
+// 18 remove
+// 19 indexOf
+// 20 lastIndexOf
+// 21 increaseCapacity
+// 22 ensureCapacity
+// 23 trimToSize
+
+package ru.academit.arrayList;
+
+import java.util.*;
+
+public class ArrayList<T> implements List<T> {
+
+    private T[] items; // length = длина,  items.length = вместимость, длина массива
+    private int length; // реальное кол-во элементов, которые существуют в списке, 0 - by default, длина списка
+    private int modCount;
+
+    public ArrayList(int initialCapacity) {
+        //noinspection unchecked
+        items = (T[]) new Object[initialCapacity];
+    }
+
+    @Override
+    public int size() {
+        return length;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        if (o == null) {
+            for (T arrayItem : items) {
+                if (arrayItem == null) {
+                    return true;
+                }
+            }
+        } else {
+            for (T arrayItem : items) {
+                if (arrayItem == null) {
+                    return false;
+                } else if (arrayItem.equals(o)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private class ArrayListIterator implements Iterator<T> {
+        private int currentIndex = -1;
+        private int currentModCount = modCount;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex + 1 < length;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("The end of collection is reached");
+            } else if (currentModCount != modCount) {
+                throw new ConcurrentModificationException("The elements were changed");
+            }
+            currentIndex++;
+            return items[currentIndex];
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayListIterator();
+    }
+
+    public Object[] toArray() {
+        return Arrays.copyOf(items, length);
+    }
+
+    public <T> T[] toArray(T[] a) {
+        if (a.length < length)
+            //noinspection unchecked
+            return (T[]) Arrays.copyOf(items, length, a.getClass());
+        System.arraycopy(items, 0, a, 0, length);
+        if (a.length > length)
+            a[length] = null;
+        return a;
+    }
+
+    @Override
+    public boolean add(T t) {
+        if (length >= items.length) {
+            increaseCapacity();
+        }
+        items[length] = t;
+        length++;
+        modCount++;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        int count = -1;
+        if (o == null) {
+            for (T arrayItem : items) {
+                if (arrayItem == null) {
+                    remove(count);
+                    return true;
+                }
+            }
+        } else {
+            for (T arrayItem : items) {
+                count++;
+                if (arrayItem.equals(o)) {
+                    remove(count);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        if (c != null) {
+            int count = 0;
+            for (Object cElement : c) {
+                for (T arrayItem : items) {
+                    if (arrayItem == null) {
+                        break;
+                    } else if (arrayItem.equals(cElement)) {
+                        count++;
+                    }
+                }
+            }
+            return count == c.size();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        if (c != null) {
+            for (T cElement : c) {
+                add(cElement);
+                modCount++;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        if (index > length - 1) {
+            throw new ArrayIndexOutOfBoundsException("Your index is greater than list's length" + System.lineSeparator() +
+                    "Specify another index or paste more elements into the list to increase capacity...");
+        } else if (c != null) {
+            ensureCapacity(length + c.size());
+            System.arraycopy(items, index, items, index + c.size(), length - c.size());
+            int count = 0;
+            int indexCurrent = index;
+            for (T cElement : c) {
+                if (count < c.size()) {
+                    items[indexCurrent] = cElement;
+                    count++;
+                    modCount++;
+                    length++;
+                    indexCurrent++;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        int count = 0;
+        for (int index = 0; index < size(); ++index) {
+            for (Object cItem : c) {
+                if (items[index].equals(cItem)) {
+                    remove(index);
+                    count++;
+                    modCount++;
+                }
+            }
+        }
+        return count > 0;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        if (c != null) {
+            int count = 0;
+            boolean isContain = false;
+            for (int index = 0; index < size(); ++index) {
+                for (Object cItem : c) {
+                    if (!items[index].equals(cItem)) {
+                        isContain = true;
+                    }
+                }
+                if (isContain) {
+                    remove(index);
+                    count++;
+                    modCount++;
+                }
+            }
+            return count > 0;
+        }
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        int tempLength = length;
+        for (int i = 0; i < tempLength; i++) {
+            remove(items[0]);
+        }
+    }
+
+    @Override
+    public T get(int index) {
+        if (index > length - 1) {
+            throw new ArrayIndexOutOfBoundsException("Your index is greater than list's length" + System.lineSeparator() +
+                    "Specify another index or paste more elements into the list to increase capacity...");
+        }
+        return items[index];
+    }
+
+    @Override
+    public T set(int index, T element) {
+        if (index > length - 1) {
+            throw new ArrayIndexOutOfBoundsException("Your index is greater than list's length" + System.lineSeparator() +
+                    "Specify another index or paste more elements into the list to increase capacity...");
+        }
+        items[index] = element;
+        return items[index];
+    }
+
+    @Override
+    public void add(int index, T element) {
+        if (index > length) {
+            throw new ArrayIndexOutOfBoundsException("Your index is greater than list's length" + System.lineSeparator() +
+                    "Specify another index or paste more elements into the list to increase capacity...");
+        } else if (index < length) {
+            if (length >= items.length) {
+                increaseCapacity();
+            }
+            System.arraycopy(items, index, items, index + 1, length - index);
+            items[index] = element;
+            length++;
+            modCount++;
+        }
+        if (index == length) {
+            add(element);
+        }
+    }
+
+    @Override
+    public T remove(int index) {
+        T prev = null;
+        if (index > length - 1) {
+            throw new ArrayIndexOutOfBoundsException("Your index is greater than list's length");
+        } else if (index < length - 1) {
+            prev = items[index];
+            System.arraycopy(items, index + 1, items, index, length - index - 1);
+        } else {    // index == length - 1
+            items = Arrays.copyOf(items, length);
+        }
+        length--;
+        modCount++;
+        return prev;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        if (o == null) {
+            for (int i = 0; i < length; i++) {
+                if (items[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < length; i++) {
+                if (items[i].equals(o)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        if (o == null) {
+            for (int i = length - 1; i > 0; i--) {
+                if (items[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = length - 1; i > 0; i--) {
+                if (o.equals(items[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private void increaseCapacity() {
+        items = Arrays.copyOf(items, items.length * 2);
+    }
+
+    public void ensureCapacity(int capacity) {
+        if (items.length >= length) {
+            items = Arrays.copyOf(items, capacity + 1);
+        }
+    }
+
+    public void trimToSize() {
+        if (items.length > length) {
+            items = Arrays.copyOf(items, length);
+        } else {
+            throw new IllegalStateException("relax, there's no need to trim this array :] ");
+        }
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
---
+++ ArrayList/src/ru/academit/arrayList/main/Main.java
@@ -0,0 +1,218 @@
+package ru.academit.arrayList.main;
+
+import ru.academit.arrayList.ArrayList;
+
+import java.util.Arrays;
+
+//@Test
+
+// 1 size
+// 2 isEmpty
+// 3 contains
+// 4 iterator
+// 5 toArray
+// 6 toArray
+// 7 add
+// 8 remove
+// 9 containsAll
+// 10 addAll
+// 11 addAll
+// 12 removeAll
+// 13 retainAll
+// 14 clear
+// 15 get
+// 16 set
+// 17 add
+// 18 remove
+// 19 indexOf
+// 20 lastIndexOf
+// 21 increaseCapacity
+// 22 ensureCapacity
+// 23 trimToSize
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> stringList = new ArrayList<>(15);
+        stringList.add("FFFF");
+        stringList.add("something");
+        stringList.add("aaaaaa");
+        stringList.add("???");
+        stringList.add("cccccc");
+
+        ArrayList<Integer> intList = new ArrayList<>(1);
+        intList.add(10);
+        intList.add(66);
+        intList.add(777);
+        intList.add(223);
+        intList.add(3);
+        intList.add(0);
+        intList.add(-5);
+
+        String[] sss = stringList.toArray(new String[stringList.size()]);
+        Integer[] aaa = intList.toArray(new Integer[intList.size()]);
+        System.out.println(Arrays.toString(sss));
+        System.out.println(Arrays.toString(aaa));
+
+        System.out.println();
+        System.out.println("SIZE");
+        System.out.println(stringList.size());
+        System.out.println(intList.size());
+
+        System.out.println();
+        System.out.println("IS_EMPTY");
+        System.out.println(stringList.isEmpty());
+        System.out.println(intList.isEmpty());
+
+        System.out.println();
+        System.out.println("CONTAINS");
+        System.out.println(Arrays.toString(sss));
+        System.out.println(Arrays.toString(aaa));
+        System.out.println(stringList.contains("aaaaaa"));
+        System.out.println(intList.contains(223));
+
+        System.out.println();
+        System.out.println("CONTAINS_ALL");
+        ArrayList<String> stringList2 = new ArrayList<>(5);
+        stringList2.add("FFFF");
+        stringList2.add("aaaaaa");
+        System.out.println(stringList.containsAll(stringList2));
+
+        System.out.println();
+        System.out.println("ADD");
+        System.out.println(stringList.add("NEW"));
+        System.out.println(intList.add(1));
+        String[] sss2 = stringList.toArray(new String[stringList.size()]);
+        Integer[] aaa2 = intList.toArray(new Integer[intList.size()]);
+        System.out.println(Arrays.toString(sss2));
+        System.out.println(Arrays.toString(aaa2));
+
+        System.out.println();
+        System.out.println("REMOVE_BY_DATA");
+        System.out.println(stringList.remove("something"));
+        System.out.println(intList.remove((Integer) 22));
+        String[] sss3 = stringList.toArray(new String[stringList.size()]);
+        Integer[] aaa3 = intList.toArray(new Integer[intList.size()]);
+        System.out.println(Arrays.toString(sss3));
+        System.out.println(Arrays.toString(aaa3));
+
+        System.out.println();
+        System.out.println("REMOVE_BY_INDEX");
+        System.out.println(stringList.remove(3));
+        System.out.println(intList.remove(7));
+        String[] sss4 = stringList.toArray(new String[stringList.size()]);
+        Integer[] aaa4 = intList.toArray(new Integer[intList.size()]);
+        System.out.println(Arrays.toString(sss4));
+        System.out.println(Arrays.toString(aaa4));
+
+        System.out.println();
+        System.out.println("ADD_BY_INDEX");
+        stringList.add(3, "A_LIST");
+        intList.add(7, -19);
+        String[] s4 = stringList.toArray(new String[stringList.size()]);
+        Integer[] a4 = intList.toArray(new Integer[intList.size()]);
+        System.out.println(Arrays.toString(s4));
+        System.out.println(Arrays.toString(a4));
+
+        System.out.println();
+        System.out.println("INDEX_OF");
+        System.out.println(stringList.indexOf("???"));
+        System.out.println(stringList.indexOf("A_LIST"));
+        System.out.println(intList.indexOf(-19));
+        System.out.println(intList.indexOf(11));
+
+        System.out.println(stringList.add("A_LIST"));
+        System.out.println(intList.add(-19));
+        System.out.println();
+
+        System.out.println("LAST_INDEX_OF");
+        System.out.println(stringList.lastIndexOf("FF2FF"));
+        System.out.println(stringList.lastIndexOf("A_LIST"));
+        System.out.println(intList.lastIndexOf(-19));
+        System.out.println(intList.lastIndexOf(11));
+
+        System.out.println();
+        String[] s5 = stringList.toArray(new String[stringList.size()]);
+        Integer[] a5 = intList.toArray(new Integer[intList.size()]);
+        System.out.println(Arrays.toString(s5));
+        System.out.println(Arrays.toString(a5));
+
+        System.out.println();
+        System.out.println("GET/SET");
+        System.out.println(stringList.get(5));
+        System.out.println(intList.get(7));
+
+        System.out.println(stringList.set(5, "SET"));
+        System.out.println(intList.set(7, 2018));
+        System.out.println(stringList.get(5));
+        System.out.println(intList.get(7));
+        String[] s6 = stringList.toArray(new String[stringList.size()]);
+        Integer[] a6 = intList.toArray(new Integer[intList.size()]);
+        System.out.println(Arrays.toString(s6));
+        System.out.println(Arrays.toString(a6));
+
+        System.out.println();
+        System.out.println("ADD_ALL");
+        ArrayList<String> newCollection = new ArrayList<>(5);
+        newCollection.add("smth");
+        newCollection.add("AAA");
+        newCollection.add("ZZZ");
+
+        stringList.addAll(newCollection);
+        String[] addAll = stringList.toArray(new String[stringList.size()]);
+        System.out.println(Arrays.toString(addAll));
+
+        ArrayList<String> newCollection2 = new ArrayList<>(10);
+        newCollection2.add("!___!");
+        newCollection2.add(":)");
+        newCollection2.add(":<");
+        newCollection2.add("aaaaaa");
+        stringList.add("aaaaaa");
+
+        String[] arrr = newCollection2.toArray(new String[newCollection2.size()]);
+        System.out.println(Arrays.toString(arrr));
+
+        stringList.addAll(4, newCollection2);
+        String[] addAll2 = stringList.toArray(new String[stringList.size()]);
+        System.out.println(Arrays.toString(addAll2));
+
+        System.out.println();
+        System.out.println("REMOVE_ALL");
+        String[] test1 = stringList.toArray(new String[stringList.size()]);
+        System.out.println(Arrays.toString(test1));
+        System.out.println(Arrays.toString(arrr));
+        System.out.println(stringList.removeAll(newCollection2));
+
+        String[] test2 = stringList.toArray(new String[stringList.size()]);
+        System.out.println(Arrays.toString(test2));
+
+        System.out.println();
+        System.out.println("RETAIN_ALL");
+        String[] test3 = stringList.toArray(new String[stringList.size()]);
+        System.out.println(Arrays.toString(test3));
+        ArrayList<String> newCollection3 = new ArrayList<>(10);
+        newCollection3.add("???");
+        newCollection3.add("AAA");
+        newCollection3.add("ZZZ");
+        newCollection3.add("NEW");
+        String[] ar2 = newCollection3.toArray(new String[newCollection2.size()]);
+        System.out.println(Arrays.toString(ar2));
+        System.out.println(stringList.retainAll(newCollection3));
+        String[] test4 = stringList.toArray(new String[stringList.size()]);
+        System.out.println(Arrays.toString(test4));
+
+        System.out.println();
+        System.out.println(Arrays.toString(test4));
+        Integer[] test5 = intList.toArray(new Integer[intList.size()]);
+        System.out.println(Arrays.toString(test5));
+
+        System.out.println("CLEAR");
+        stringList.clear();
+        intList.clear();
+        String[] clear1 = stringList.toArray(new String[stringList.size()]);
+        Integer[] clear2 = intList.toArray(new Integer[intList.size()]);
+        System.out.println(Arrays.toString(clear1));
+        System.out.println(Arrays.toString(clear2));
+        System.out.println(stringList.isEmpty());
+    }
+}
